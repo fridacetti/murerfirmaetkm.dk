d557d43e83fb168819294df57894799a5cb2b170
Nannasarah <nannasp01@gmail.com>
2026-05-24 21:31:06 +0200

    Revert "Merge branch 'main' into styling-nans"
    This reverts commit 3335826128496b43d13ebd64168d93e8953ebae5, reversing
    changes made to 53a8d5113b7fd24916e4bbbf436827e1cc41289f.

--- src/components/VaerdiCard.tsx
+++ src/components/VaerdiCard.tsx
@@ -1,35 +1,4 @@
 import { storyblokEditable } from "@storyblok/react";
-import { FiHome } from "react-icons/fi";
-import { GiTrowel } from "react-icons/gi";
-
-const getIcon = (title?: string) => {
-  if (title?.toLowerCase() === "håndværk") {
-    return GiTrowel;
-  }
-
-  return FiHome;
-};
-
-export default function VaerdiCard({ blok }: { blok: any }) {
-  const Icon = getIcon(blok.title);
-
-  return (
-    <article
-      {...storyblokEditable(blok)}
-      className="flex flex-col items-center text-center"
-    >
-      <div className="mb-7 flex h-28 w-28 items-center justify-center rounded bg-[#50543B] text-[#F5EDE0]">
-        <Icon className="text-6xl" />
-      </div>
-
-      <h3 className="mb-5 font-serif text-3xl font-bold text-[#282828]">
-        {blok.title}
-      </h3>
-
-      <p className="whitespace-pre-line text-xl leading-snug text-[#282828]">
-        {blok.text}
-      </p>
-    </article>
 import { FaTrowel, FaHouse } from "react-icons/fa6";
 
 const icons = {

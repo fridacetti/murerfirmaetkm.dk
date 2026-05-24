097fa5e5df9125be36599af1365d2f32ca5a60a9
Nannasarah <nannasp01@gmail.com>
2026-05-24 21:01:19 +0200

    footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { initStoryblok } from "@/lib/storyblok";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 initStoryblok();
 
 export const metadata: Metadata = {
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );

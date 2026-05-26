36f2f174c155bcc57f4228ab500e5714cf87b6ac
Nannasarah <nannasp01@gmail.com>
2026-05-26 15:37:58 +0200

    kontaktside

--- src/components/Formular.tsx
+++ src/components/Formular.tsx
@@ -1,11 +1,26 @@
 "use client";
+import { StoryblokComponent, storyblokEditable } from "@storyblok/react";
 import Form from "./Form";
 
-export default function Formular() {
+export default function Formular({ blok }: { blok: any }) {
+  const kontaktInfoItems = blok.kontaktinfo || blok.kontaktInfo || blok.KontaktInfo || [];
+
   return (
-    <div className="bg-[#545943] text-white px-8 py-16 md:px-20 lg:px-36">
-      <h2 className="mb-4">Send os en besked</h2>
-      <Form />
+    <div className="grid md:grid-cols-2">
+      <div {...storyblokEditable(blok)} className="bg-[#545943] text-white px-8 py-16 md:px-20 lg:px-36">
+        <h2 className="mb-4">Send os en besked</h2>
+        <Form />
+      </div>{" "}
+      <div>
+        {blok.inkluderet?.map((inkluderet: any) => (
+          <StoryblokComponent key={inkluderet._uid} blok={inkluderet} />
+        ))}
+      </div>
+      <div>
+        {kontaktInfoItems.map((kontaktinfo: any) => (
+          <StoryblokComponent key={kontaktinfo._uid} blok={kontaktinfo} />
+        ))}
+      </div>
     </div>
   );
 }

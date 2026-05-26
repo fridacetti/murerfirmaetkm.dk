a6cf3672bbc8ed398c938faa7f66ec38e4ece075
fridacetti <frjo0003@stud.kea.dk>
2026-05-26 21:26:27 +0200

    feat: update layout and functionality of Formular and Inkluderet components; enhance YdelserCard link handling

--- src/components/Formular.tsx
+++ src/components/Formular.tsx
@@ -7,27 +7,30 @@ import Inkluderet from "./Inkluderet";
 
 export default function Formular({ blok }: { blok: any }) {
   return (
-    <section {...storyblokEditable(blok)} className="grid grid-cols-1 md:grid-cols-2">
+    <section {...storyblokEditable(blok)} className="grid grid-cols-1 md:grid-cols-2 bg-[#F5EDE0]">
       {/* Venstre side */}
-      <div className="bg-[#545943] text-white px-8 py-16 md:px-20 lg:px-36">
-        <h2 className="mb-4 text-3xl font-bold">Send os en besked</h2>
+      <div className="bg-[#545943] text-white px-6 py-12 md:px-20 lg:px-36">
+        <h2 className="mb-4 text-2xl md:text-3xl font-bold text-center md:text-left">Send os en besked</h2>
         <Form />
       </div>
 
-      {/* Højre side */}
-      <div
-        className="flex flex-col gap-8 md:ml-[159px] overflow-hidden "
-      >
-        {/* Kontaktinfo */}
-        {blok.kontaktinfo?.map((item: any) => (
-          <KontaktInfo key={item._uid} blok={item} />
-        ))}
+    {/* Højre side */}
+      <div className="flex flex-col gap-8 md:ml-[159px] px-8 py-10">
 
-        {/* Inkluderet */}
-        {blok.inkluderet?.map((item: any) => (
-          <Inkluderet key={item._uid} blok={item} />
-        ))}
-      </div>
+        {/* Inkluderet – først på mobil, sidst på desktop */}
+        <div className="order-1 md:order-2">
+          {blok.inkluderet?.map((item: any) => (
+            <Inkluderet key={item._uid} blok={item} />
+          ))}
+        </div>
+
+        {/* Kontaktinfo – sidst på mobil, først på desktop */}
+        <div className="order-2 md:order-1">
+          {blok.kontaktinfo?.map((item: any) => (
+            <KontaktInfo key={item._uid} blok={item} />
+          ))}
+        </div>
+        </div>
     </section>
   );
 }
--- src/components/Inkluderet.tsx
+++ src/components/Inkluderet.tsx
@@ -2,7 +2,7 @@ import { storyblokEditable } from "@storyblok/react";
 
 export default function Inkluderet({ blok }: { blok: any }) {
   return (
-    <section {...storyblokEditable(blok)} className="bg-tertiary rounded-l-lg h-[379px] p-6 -mr-8 md:-mr-[155px] pr-4 md:pr-[155px] mb-6">
+    <section {...storyblokEditable(blok)} className="md:bg-tertiary rounded-l-lg md:h-[379px] md:p-6 -mr-8 md:-mr-[155px] pr-4 md:pr-[155px] mb-6">
       <h2 className="subtitle mb-4">{blok.subtitle}</h2>
 
       <p className="bodytext">{blok.text}</p>
